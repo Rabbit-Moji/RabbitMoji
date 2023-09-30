2c6632709d68352b6c1db5aa6ffe535e5322caa2
Rabbit-Moji <146405476+Rabbit-Moji@users.noreply.github.com>
2023-09-30 17:14:07 +0630

    Update aa.py

--- aa.py
+++ aa.py
@@ -1,28 +1,14 @@
 from gtts import gTTS
 import os
-import time
 
-# Read the content of "moji.txt"
-with open("moji.txt", "r") as file:
-    text = file.read()
+# Text to be spoken
+text = "Hello user, welcome to my channel"
 
-# Convert text to speech
-tts = gTTS("Hello users, welcome to my Facebook clone tool")
-tts.save("output.mp3")
+# Create a gTTS object
+tts = gTTS(text)
 
-# Play the generated audio
-os.system("mpv output.mp3")
+# Save the audio file
+tts.save("welcome.mp3")
 
-def loading_animation():
-    frames = [
-        "⠔⠋⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠃⠀⠉⠉⠉⠀⠀⠀⠀⠀⠀⣼⣿⠀⠀⠀⠀⠀⠀⣿⣧⠀⠀⠀⠀⠀⠈⠉⠉⠉⠀⠘⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠙⠢",
-        # Add more frames here if needed
-    ]
-
-    for frame in frames:
-        os.system("clear")  # Clear the terminal before printing the new frame
-        print(frame)
-        time.sleep(0.1)
-
-loading_animation()
-print("\nChecking for updates...")  # Updated the message for clarity
+# Play the audio using a Termux command
+os.system("termux-media-player play welcome.mp3")
